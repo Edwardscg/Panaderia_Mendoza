765952577293ff51cc66b9cdc09c75dd3e9860f8
Edward <Edward@AN515-57>
2026-04-11 09:40:29 -0500

    Creacion de proyecto: bakeryapp-backend

---
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/BakeryappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mendozabakery.bakeryappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryappBackendApplication.class, args);
+    }
+
+}
---
+++ bakeryapp-backend/src/test/java/com/mendozabakery/bakeryappbackend/BakeryappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendozabakery.bakeryappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
