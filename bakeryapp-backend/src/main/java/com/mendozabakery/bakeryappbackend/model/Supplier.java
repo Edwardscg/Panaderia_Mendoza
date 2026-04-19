48305cc361a0ddaa1d8d329ff20980eb93c7d483
Edward <Edward@AN515-57>
2026-04-19 13:01:49 -0500

    feat: crear clase Supplier

---
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/model/Supplier.java
@@ -0,0 +1,38 @@
+package com.mendozabakery.bakeryappbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idSupplier;
+
+    @Column(nullable = false, length = 100)
+    private String businessName;
+
+    @Column(nullable = false, length = 11)
+    private String ruc;
+
+    @Column(nullable = false, length = 10)
+    private String phone;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 150)
+    private String address;
+
+    @Column(nullable = false)
+    private boolean status;
+}
