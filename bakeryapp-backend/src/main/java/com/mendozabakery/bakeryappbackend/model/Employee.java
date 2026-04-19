a90808d5d5b18bcfa9f765b460363f273c5ada02
Dante7481 <mirkoschunque@gmail.com>
2026-04-18 19:50:13 -0500

    refactor: standardize Employee and User_Account classes with consistent annotations and structure

--- bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/model/Employee.java
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/model/Employee.java
@@ -1,5 +1,31 @@
 package com.mendozabakery.bakeryappbackend.model;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class Employee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idUse_Account;
+
+    @Column(nullable = false, length = 70)
+    private String nameUser_Account;
+
+    @Column(nullable = false)
+    private boolean status;
 }
--- bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/model/User_Account.java
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/model/User_Account.java
@@ -1,5 +1,30 @@
 package com.mendozabakery.bakeryappbackend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class User_Account {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idUse_Account;
+
+    @Column(nullable = false, length = 70)
+    private String nameUser_Account;
+
+    @Column(nullable = false)
+    private boolean status;
 }
