63a2260f573eecf4dd75e4371da5665d5e5b7988
Edward <Edward@AN515-57>
2026-04-19 15:02:50 -0500

    feat: agregar CRUD API para Employee (repository, service, controller)

---
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.mendozabakery.bakeryappbackend.controller;
+
+import com.mendozabakery.bakeryappbackend.model.Employee;
+import com.mendozabakery.bakeryappbackend.service.IEmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final IEmployeeService service;
+
+    @GetMapping
+    public List<Employee> findAll() throws Exception{
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Integer id) throws Exception{
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Employee save(@RequestBody Employee employee) throws Exception{
+        return service.save(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee update(@RequestBody Employee employee, @PathVariable("id") Integer id) throws Exception{
+        return service.update(employee, id);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Integer id) throws Exception{
+        service.delete(id);
+    }
+}
