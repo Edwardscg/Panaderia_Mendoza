dae537b25e019db34c2d9874a3c3c5dce44348e7
Juan Carlos <juancax@gmail.com>
2026-04-21 08:45:59 -0500

    feat: agregar modelo Inventory service y controller

---
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/controller/InventoryController.java
@@ -0,0 +1,43 @@
+package com.mendozabakery.bakeryappbackend.controller;
+
+import java.util.List;
+
+import com.mendozabakery.bakeryappbackend.model.Inventory;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.RequiredArgsConstructor;
+
+import com.mendozabakery.bakeryappbackend.service.IInventoryService;
+
+@RestController
+@RequestMapping("/inventories")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final IInventoryService service;
+
+    @GetMapping
+    public List<Inventory> findAll() throws Exception {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Inventory findById(@PathVariable("id") Integer id) throws Exception {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Inventory save(@RequestBody Inventory inventory) throws Exception {
+        return service.save(inventory);
+    }
+
+    @PutMapping("/{id}")
+    public Inventory update(@RequestBody Inventory inventory, @PathVariable("id") Integer id) throws Exception {
+        return service.update(inventory, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) throws Exception {
+        service.delete(id);
+    }
+}
