75229423f3d18ef3fa5f910ba7c9812af69d3973
Dante7481 <mirkoschunque@gmail.com>
2026-05-23 22:48:28 -0500

    feat: implement ProductCategoryDTO and update ProductCategoryController and ProductCategoryService for improved data handling

--- bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/service/IProductCategoryService.java
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/service/IProductCategoryService.java
@@ -3,15 +3,15 @@ package com.mendozabakery.bakeryappbackend.service;
 import java.util.List;
 import com.mendozabakery.bakeryappbackend.model.ProductCategory;
 
-public interface IProductCategoryService {
+public interface IProductCategoryService extends IGenericService<ProductCategory, Integer> {
 
-    ProductCategory save(ProductCategory productCategory) throws Exception;
+    //ProductCategory save(ProductCategory productCategory) throws Exception;
 
-    ProductCategory update(ProductCategory productCategory, Integer id) throws Exception;
+    //ProductCategory update(ProductCategory productCategory, Integer id) throws Exception;
 
-    List<ProductCategory> findAll() throws Exception;
+   // List<ProductCategory> findAll() throws Exception;
 
-    ProductCategory findById(Integer id) throws Exception;
+   // ProductCategory findById(Integer id) throws Exception;
 
-    void delete(Integer id) throws Exception;
+    //void delete(Integer id) throws Exception;
 }
