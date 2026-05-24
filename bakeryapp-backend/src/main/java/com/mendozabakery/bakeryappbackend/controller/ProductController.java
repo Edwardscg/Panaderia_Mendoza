64dcba8398e1007e9bbbdb045d14d975f99b74fc
Dante7481 <mirkoschunque@gmail.com>
2026-05-23 22:33:04 -0500

    feat: implement ProductDTO and update ProductController to use DTOs for better data handling

--- bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/controller/ProductController.java
+++ bakeryapp-backend/src/main/java/com/mendozabakery/bakeryappbackend/controller/ProductController.java
@@ -1,41 +1,73 @@
 package com.mendozabakery.bakeryappbackend.controller;
 
+import com.mendozabakery.bakeryappbackend.dto.ProductDTO;
 import com.mendozabakery.bakeryappbackend.model.Product;
 import com.mendozabakery.bakeryappbackend.service.IProductService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ProductController {
+
     private final IProductService service;
+    private final ModelMapper modelMapper;
 
     @GetMapping
-    public List<Product> findAll() throws Exception{
-        return service.findAll();
+    public ResponseEntity<List<ProductDTO>> findAll() throws Exception {
+
+        List<ProductDTO> list = service.findAll()
+                .stream()
+                .map(e -> modelMapper.map(e, ProductDTO.class))
+                .toList();
+
+        return ResponseEntity.ok(list);
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable("id") Integer id) throws Exception{
-        return service.findById(id);
+    public ResponseEntity<Product> findById(@PathVariable Integer id) throws Exception {
+
+        Product obj = service.findById(id);
+
+        return ResponseEntity.ok(obj);
     }
 
     @PostMapping
-    public Product save(@RequestBody Product product) throws Exception{
-        return service.save(product);
+    public ResponseEntity<Void> save(@RequestBody ProductDTO dto) throws Exception {
+
+        Product obj = service.save(modelMapper.map(dto, Product.class));
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getIdProduct())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
-    public Product update(@RequestBody Product product, @PathVariable("id") Integer id) throws Exception{
-        return service.update(product, id);
+    public ResponseEntity<ProductDTO> update(@RequestBody ProductDTO dto,
+                                             @PathVariable Integer id) throws Exception {
+
+        Product obj = service.update(modelMapper.map(dto, Product.class), id);
+
+        return ResponseEntity.ok(modelMapper.map(obj, ProductDTO.class));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id) throws Exception{
+    public ResponseEntity<Void> delete(@PathVariable Integer id) throws Exception {
+
         service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
